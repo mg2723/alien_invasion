18a62d6071c752136cff2a76642556c171177f9b
guoge2723 <guoge2723@163.com>
2018-06-22 20:33:37 +0800

    alien_invasion

---
+++ game_stats.py
@@ -0,0 +1,28 @@
+# coding:gbk
+import json
+
+
+class GameStats(object):
+    """跟踪游戏的统计信息"""
+
+    def __init__(self, ai_settings):
+        """初始化统计信息"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+
+        # 让游戏一直处于非活动状态
+        self.game_active = False
+
+        # 在任何情况下都不应重置最高得分
+        filename = 'high_score.json'
+        try:
+            with open(filename) as f_obj:
+                self.high_score = json.load(f_obj)
+        except FileNotFoundError:
+            self.high_score = 0
+
+    def reset_stats(self):
+        """初始化在游戏运行期间可能变化的统计信息"""
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
